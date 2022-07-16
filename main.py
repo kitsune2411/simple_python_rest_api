88593ab1a0330c4e17d4170802ecf2a288cc916f
Agung awan <0kitsune24@gmail.com>
2022-07-16 19:53:15 +0800

    production mode option

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, make_response
+# from waitress import serve
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,3 +9,5 @@ def hello():
     return "Hello World"
 
 app.run()
+# for production
+# serve(app, host='0.0.0.0', port=8080)
